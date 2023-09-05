a6cefe5cb7aa05a2ecc75c30e4aec082f348ed61
korelss7 <pajita200305@gmail.com>
2023-09-04 20:48:22 -0500

    React initial project, components & elements JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './platzi.webp';
-import './App.css';
+const TodoItem = () => (
+  <li>
+    <span>✔️</span>
+    <p>Botar la basura</p>
+    <span>❌</span>
+  </li>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TodoItem />;
 }
-
-export default App;
+export { App };
