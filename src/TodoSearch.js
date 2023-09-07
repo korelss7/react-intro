d6595f4e89ef6f474a62b15ee35bcfbc0587d649
korelss7 <pajita200305@gmail.com>
2023-09-07 18:17:47 -0500

    useState, using states and set states

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const TodoSearch = () => {
+  const [search, setSearch] = useState("");
   return (
     <input
+      value={search}
       placeholder="Cortar Cebollas"
       className="bg-zinc-700 placeholder-stone-400 text-white text-center border-2 border-zinc-700 outline-none p-2 rounded-xl w-64 sm:w-96 text-base font-medium hover:shadow-md hover:shadow-neutral-500 transition-shadow focus:border-white focus:shadow-neutral-500 focus:shadow-md"
-      onChange={(e) => console.log(e.target.value)}
+      onChange={(event) => {
+        setSearch(event.target.value);
+      }}
     ></input>
   );
 };
