8e0caebeee6390521e81cc4b1cb3b700cbd21b19
korelss7 <pajita200305@gmail.com>
2023-09-07 16:02:07 -0500

    Events React, onClick() onChange()

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,6 +3,7 @@ const TodoSearch = () => {
     <input
       placeholder="Cortar Cebollas"
       className="bg-zinc-700 placeholder-stone-400 text-white text-center border-2 border-zinc-700 outline-none p-2 rounded-xl w-64 sm:w-96 text-base font-medium hover:shadow-md hover:shadow-neutral-500 transition-shadow focus:border-white focus:shadow-neutral-500 focus:shadow-md"
+      onChange={(e) => console.log(e.target.value)}
     ></input>
   );
 };
