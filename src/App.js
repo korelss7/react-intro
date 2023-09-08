008857c6a3e1f96a37a60b380b44b1724a9c8208
korelss7 <pajita200305@gmail.com>
2023-09-07 21:03:36 -0500

    props parents to childrens & counting TODOs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TodoCounter } from "./TodoCounter";
 import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
@@ -7,16 +8,23 @@ import { CreateTodoButton } from "./CreateTodoButton";
 const defaultTodos = [
   { task: "Cortar cebolla", completed: true },
   { task: "Comprar comida china del mercado en media hora", completed: false },
-  { task: "Hacer la ultima tarea", completed: true },
+  { task: "Hacer la ultima tarea", completed: false },
   { task: "Praticar para el partido", completed: true },
+  { task: "Praticar para el torneo", completed: true },
 ];
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [todos, setTodos] = useState(defaultTodos);
+  const completedTodos = todos.filter((e) => e.completed).length;
+  const totalTodos = todos.length;
+
+  console.log(completedTodos);
   return (
     <article className="bg-stone-800 w-full min-h-screen text-white flex flex-col items-center font-montserrat scroll-smooth">
       <section className="flex flex-col items-center gap-3 mt-6 mb-4">
-        <TodoCounter completed={4} total={10} />
-        <TodoSearch />
+        <TodoCounter completed={completedTodos} total={totalTodos} />
+        <TodoSearch searchValue={search} setSearchValue={setSearch} />
       </section>
 
       <TodoList>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,14 +1,11 @@
-import { useState } from "react";
-
-const TodoSearch = () => {
-  const [search, setSearch] = useState("");
+const TodoSearch = ({ searchValue, setSearchValue }) => {
   return (
     <input
-      value={search}
+      value={searchValue}
       placeholder="Cortar Cebollas"
       className="bg-zinc-700 placeholder-stone-400 text-white text-center border-2 border-zinc-700 outline-none p-2 rounded-xl w-64 sm:w-96 text-base font-medium hover:shadow-md hover:shadow-neutral-500 transition-shadow focus:border-white focus:shadow-neutral-500 focus:shadow-md"
       onChange={(event) => {
-        setSearch(event.target.value);
+        setSearchValue(event.target.value);
       }}
     ></input>
   );
